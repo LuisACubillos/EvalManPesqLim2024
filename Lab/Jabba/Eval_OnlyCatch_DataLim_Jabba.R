fcc821b5b51d28f7df2fcf7516e2fc38039a4cd7
LuisACubillos <lucubillos@gmail.com>
2024-01-18 00:10:01 -0300

    cambios script jabba

--- Lab/Jabba/Eval_OnlyCatch_DataLim_Jabba.R
+++ Lab/Jabba/Eval_OnlyCatch_DataLim_Jabba.R
@@ -1,19 +1,18 @@
+#install.packages(devtools)
+#devtools::install_github("jabbamo1del/JABBA")
+
 library(JABBA)
 
-File = "~/01Cursos/RBP/CasoEstudio/Eval_congrio_colorado/JabbaSAM"
-
-
+File = "/Users/luiscubillos/01Cursos/EvalManPesqLim2024/Lab/Jabba"
 
 # Datos -------------------------------------------------------------------
 dt <- read.csv("Datos/congriocol.csv",sep=";")
 names(dt)
 
-
 assessment = "Congrio_col"
 output.dir = file.path(File,assessment)
 dir.create(output.dir,showWarnings = F)
 setwd(output.dir)
-
 
 # Prepara los datos de huepo ----------------------------------------------
 catch = dt[,c(2,3)]
@@ -32,17 +31,20 @@ jbinput = build_jabba(catch=congrio$catch,
                       b.prior=c(0.2,0.3,2002,"bk"),
                       psi.prior = c(0.5,0.3))
 # Ajuste JABBA
-ccol0 = fit_jabba(jbinput,save.jabba=TRUE,output.dir=output.dir)
+ccol0 = fit_jabba(jbinput,
+                  save.jabba=TRUE,
+                  output.dir=output.dir)
 # Make individual plots
+jbplot_summary(ccol0)
 jbplot_catch(ccol0)
 jbplot_catcherror(ccol0)
 jbplot_ppdist(ccol0)
 jbplot_mcmc(ccol0)
 jbplot_procdev(ccol0)
 jbplot_trj(ccol0,type="B",add=F)
-jbplot_trj(ccol0,type="F",add=T)
-jbplot_trj(ccol0,type="BBmsy",add=T)
-jbplot_trj(ccol0,type="FFmsy",add=T)
+jbplot_trj(ccol0,type="F",add=F)
+jbplot_trj(ccol0,type="BBmsy",add=F)
+jbplot_trj(ccol0,type="FFmsy",add=F)
 
 ccol0$pars_posterior[,1]
 round(ccol0$estimates,3)
